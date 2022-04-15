f048076cef0792bd28f4f7a3fec5ec3e5b82c374
Leonardo Sanfins <leohsanfins@hotmail.com>
2022-04-15 12:45:07 -0300

    Quase terminando o exercício 4

--- src/ex4/Empregado.java
+++ src/ex4/Empregado.java
@@ -1,10 +1,21 @@
 package ex4;
 
+import java.util.Iterator;
+
+import ex3.Aluno;
+
 public class Empregado {
 
 	private String nome;
 	private String cpf;
 	private double salario;
+	
+	public Empregado(String nome, String cpf, double salario) {
+		
+		this.nome = nome;
+		this.cpf = cpf;
+		this.salario = salario;
+	}
 
 	public String getNome() {
 		return nome;
@@ -30,5 +41,11 @@ public class Empregado {
 		this.salario = salario;
 	}
 	
+	public void status() {
+		
+		System.out.println("Nome: " + this.getNome());
+		System.out.println("CPF: " + this.getCpf());
+		System.out.println("Salario: " + this.getSalario());
+	}
 	
 }
--- src/ex4/TesteEmpregado.java
+++ src/ex4/TesteEmpregado.java
@@ -6,22 +6,40 @@ public class TesteEmpregado {
 	
 	public static void main(String[] args) {
 		
-		Empregado[] listaEmpregados = new Empregado[5];
+		Empregado[] listaEmpregados = new Empregado[2];
 		Scanner sc = new Scanner(System.in);
+		Empregado em = null;
 		
 		int codigo = 0;
 		
-		for(int i = 0; i < listaEmpregados.length; i ++) {
+		for(int i = 0; i < listaEmpregados.length; i++) {
 			
 			String nome = null;
 			String cpf = null;
 			double salario = 0;
 			
 			System.out.println("Digite o nome do empregado: ");
+			nome = sc.nextLine();
+			System.out.println("Digite o CPF do empregado: ");
+			cpf = sc.nextLine();
+			System.out.println("Digite o salário do empregado: ");
+			salario = sc.nextDouble();
 			
+			
+			em = new Empregado(nome, cpf, salario);
+			
+			listaEmpregados[i] = em;
+			
+			sc.nextLine();
 		}
 		
 		
 		
+		
+		
+		
 	}
+	
 }
+
+
