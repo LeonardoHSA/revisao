a385ca3aa41483ba0ab59b0a8e234aefb3a6a7b1
Leonardo Sanfins <leohsanfins@hotmail.com>
2022-04-15 10:46:20 -0300

    Começando o exercício 4

---
+++ src/ex4/TesteEmpregado.java
@@ -0,0 +1,27 @@
+package ex4;
+
+import java.util.*;
+
+public class TesteEmpregado {
+	
+	public static void main(String[] args) {
+		
+		Empregado[] listaEmpregados = new Empregado[5];
+		Scanner sc = new Scanner(System.in);
+		
+		int codigo = 0;
+		
+		for(int i = 0; i < listaEmpregados.length; i ++) {
+			
+			String nome = null;
+			String cpf = null;
+			double salario = 0;
+			
+			System.out.println("Digite o nome do empregado: ");
+			
+		}
+		
+		
+		
+	}
+}
